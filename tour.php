d9d53391f481404912a83a3d24ef5029ae38c5e1
kelvinramadan <kelvinramadan2@gmail.com>
2024-10-31 13:29:11 +0700

    -

--- tour.php
+++ tour.php
@@ -108,7 +108,7 @@ if(isset($_POST['reserve'])) {
 
               </div>
            </div>
-
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50782.05403468191!2d119.8630!3d-2.9726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMsKwNTcnMTQuNiJOIDEyOcKwNTUnNTcuNCJ9!5e0!3m2!1sen!2sid!4v1633679151718" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
          </div>
        </div>
 <?php endwhile; ?>
